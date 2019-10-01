eb024b86b649686c531ce13c0133c5ff9f96a83f
Miguel Moronta <miguel@moronta.com.do>
2019-10-01 12:35:08 -0400

    Se realizó PiraPong
     - Crear escenario Pong. [Cubes, Materials, Physic Materials]
     - Crear personajes que se desplazan a modo de paletas. [Sprite, Rigidbody, Collider, Axes/Touches/Mouse Position/KeyCodes, Transform.Translate, GetComponent, RigidBody.velocity, Time.deltaTime, Mathf.Clamp]
     - Crear pelota que colisiona por la pantalla. [Sphere, Collider, RigidBody.AddForce, RigidBody.velocity]
     - Implementar IA para paleta computadora. [Mathf.Lerp]
     - Zona muerta, conteo de puntaje e instanciar pelota. [Collider, 3D Text, Instantiate, Transform.SetParent, Prefab, OnTriggerEnter, Destroy, SendMessage]
     - Agregar gestor de sonidos. [Audio Source]
     - Agregar música de fondo. [Audio Source]
     - Agregar efectos de sonido. [Audio Source]
     - Permitir registrar puntaje en Servicio Web. [MVC Web API, Base de Datos]

---
+++ PirataRPG/Assets/Scripts/PiraPong/DeadZoneController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZoneController : MonoBehaviour
+{
+    public TextMesh Player1Text, Player2Text;
+    public GameObject Player1, Player2;
+    static int _score1, _score2;
+    public GameObject Ball;
+    bool _player1Scored;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name != "bomb")
+            return;
+
+        _player1Scored = gameObject.name == "Player2DeadZone";
+        Ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+        if(_player1Scored)
+        {
+            _score1++;
+            Player1Text.text = _score1.ToString();
+            Ball.transform.SetParent(Player1.transform);
+            Ball.transform.localPosition = new Vector3(1, 0);
+        }
+        else
+        {
+            _score2++;
+            Player2Text.text = _score2.ToString();
+            Ball.transform.SetParent(Player2.transform);
+            Ball.transform.localPosition = new Vector3(-1, 0);
+        }
+    }
+}
---
+++ PirataRPG/Assets/Scripts/PiraPong/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource PongSound;
+    public AudioSource GameStartSound;
+    public AudioSource ScoreSentSound;
+
+    public void PlayPongSound()
+    {
+        PongSound.Play();
+    }
+
+    public void PlayGameStartSound()
+    {
+        GameStartSound.Play();
+    }
+
+    public void PlayScoreSentSound()
+    {
+        ScoreSentSound.Play();
+    }
+}
