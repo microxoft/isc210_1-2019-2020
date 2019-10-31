2bc8cac02fd008e2adfadd575bbc43225b177d02
Miguel Moronta <miguel@moronta.com.do>
2019-10-31 11:17:49 -0400

    Taberna lista.
    Se ha programado la taberna para que cargue el minijuego seleccionado.

---
+++ PirataRPG/Assets/Scripts/Tavern/CameraPositionController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPositionController : MonoBehaviour
+{
+    public GameObject Player;
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x,
+            Mathf.Clamp(Player.transform.position.y, -2.23f, 2.23f),
+            gameObject.transform.position.z);
+    }
+}
---
+++ PirataRPG/Assets/Scripts/Tavern/MinigameLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MinigameLoader : MonoBehaviour
+{
+    public void OnTriggerStay(Collider other)
+    {   
+        if (!Input.GetButtonDown("Fire2"))
+            return;
+
+        switch(gameObject.name)
+        {
+            case "PongScene":
+                SceneManager.LoadScene("PiraPong");
+                break;
+            case "PiraEssenceScene":
+                SceneManager.LoadScene("PiraEssence");
+                break;
+            case "BrickBreakerScene":
+                //SceneManager.LoadScene("PiraPong");
+                break;
+            case "MoronPongScene":
+                //SceneManager.LoadScene("PiraPong");
+                break;
+        }
+    }
+}
