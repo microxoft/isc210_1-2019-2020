08896c4fcbb008db7aa1f25346811a5d1de2caae
Miguel Moronta <miguel@moronta.com.do>
2019-11-07 11:04:43 -0400

    Escena Menú lista
    Se crearon los distintos ítems del menú, dándoles comportamiento a cada uno, para que tengan un efecto en el mousehover. También, se programó el comportamiento de iniciar partida, salir del juego y mostrar opciones del juego. Para mostrar las opciones del juego se creó un Canvas y se diseñó una interfaz gráfica con las opciones del juego (nombre del jugador, volumen del sonido, dificultad), y se utilizó almacenamiento persistente (datacontractserializer y PlayerPrefs) para registrar las opciones en disco. Estas opciones se cargan al ejecutar el juego.

---
+++ PirataRPG/Assets/Scripts/Menu/GameOptionsLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using UnityEngine;
+
+public class GameOptionsLoader : MonoBehaviour
+{
+    string _localPath = "gameOptions.xml";
+
+    private void Start()
+    {
+        LoadGameOptions();
+
+        Debug.Log("En el playerprefs el nombre guardado es: " + PlayerPrefs.GetString("PlayerName", "Jhon Doe"));
+    }
+
+    public void SaveGameOptions()
+    {
+        using (Stream fileStream = new FileStream(Application.persistentDataPath + "\\" + _localPath, FileMode.Create))
+        {
+            DataContractSerializer dcontract = new DataContractSerializer(typeof(GameOptions));
+            dcontract.WriteObject(fileStream, GameOptions.Instance);
+        }
+    }
+
+    public void LoadGameOptions()
+    {
+        using (Stream fileStream = new FileStream(Application.persistentDataPath + "\\" + _localPath, FileMode.Open))
+        {
+            DataContractSerializer dcontract = new DataContractSerializer(typeof(GameOptions));
+            GameOptions.Instance = (GameOptions)dcontract.ReadObject(fileStream);
+        }
+    }
+}
